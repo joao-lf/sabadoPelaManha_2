b84aa3d7edef3523219c89e77716d0640e278986
joao-lf <joao_lf@live.com>
2022-10-24 22:13:32 -0300

    Primeiro Commit

---
+++ src/exercicio1.java
@@ -0,0 +1,8 @@
+public class exercicio1 {
+
+    public static void main(String[] args) {
+
+        System.out.printf("O número é " + ((Integer.parseInt(args[0]) % 2 == 0) ? "PAR" : "IMPAR"));
+
+    }
+}
